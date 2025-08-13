9b7b1d206974c8e550122efb38deb6c34583f07e
Roman Kozak <cossack.roman@gmail.com>
2025-08-14 01:21:27 +0300

    wip

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,15 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-
+    }
+    // ЦЕЙ БЛОК - КЛЮЧ ДО ВИРІШЕННЯ. ВІН ПРИМУСОВО ВСТАНОВИТЬ СУМІСНІ ВЕРСІЇ
+    plugins {
+        id("com.android.application") version "8.4.1" apply false
+        id("com.android.library") version "8.4.1" apply false
+        id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+        id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+        id("com.google.dagger.hilt.android") version "2.51.1" apply false
     }
 }
 
@@ -13,15 +21,11 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        // --- ВАЖЛИВО: Додано репозиторій Maven Central ---
         mavenCentral()
+        mavenLocal()
         maven { url = uri("https://jitpack.io") }
-
     }
 }
 
 rootProject.name = "ForwardAppMobile"
 include(":app")
-
-include(":reorderable")
-project(":reorderable").projectDir = file("libs/reorderable")
