7e156fcc9c3a8850104f5ed5ccca6976861f8f9c
Roman Kozak <cossack.roman@gmail.com>
2025-10-15 23:49:35 +0300

    refactored projrepository

--- app/src/main/java/com/romankozak/forwardappmobile/data/repository/ProjectRepository.kt
+++ app/src/main/java/com/romankozak/forwardappmobile/data/repository/ProjectRepository.kt
@@ -17,19 +17,11 @@ import java.util.UUID
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Provider
+import com.romankozak.forwardappmobile.data.repository.SearchRepository
 import javax.inject.Singleton
 
 internal enum class ContextTextAction { ADD, REMOVE }
 
-private val GlobalSearchResultItem.typeOrder: Int
-    get() =
-        when (this) {
-            is GlobalSearchResultItem.ProjectItem,
-            is GlobalSearchResultItem.SublistItem,
-            -> 0
-            is GlobalSearchResultItem.GoalItem -> 1
-            else -> 2
-        }
 
 @Singleton
 class ProjectRepository
@@ -48,6 +40,7 @@ constructor(
     private val reminderRepository: ReminderRepository,
     private val projectArtifactDao: ProjectArtifactDao,
     private val projectLogRepository: ProjectLogRepository,
+    private val searchRepository: SearchRepository,
 ) {
     private val contextHandler: ContextHandler by lazy { contextHandlerProvider.get() }
     private val TAG = "CUSTOM_LIST_DEBUG"
@@ -423,46 +416,9 @@ constructor(
 
     @Transaction
     suspend fun searchGlobal(query: String): List<GlobalSearchResultItem> {
-        val goalResults =
-            goalDao.searchGoalsGlobal(query).mapNotNull { searchResult ->
-                val listItem = listItemDao.getListItemByEntityId(searchResult.goal.id)
-                listItem?.let {
-                    GlobalSearchResultItem.GoalItem(
-                        goal = searchResult.goal,
-                        listItem = it,
-                        projectName = searchResult.projectName,
-                        pathSegments = searchResult.pathSegments
-                    )
-                }
-            }
-        val linkResults =
-            linkItemDao.searchLinksGlobal(query).map {
-                GlobalSearchResultItem.LinkItem(it)
-            }
-        val subprojectResults =
-            projectDao.searchSubprojectsGlobal(query).map {
-                GlobalSearchResultItem.SublistItem(it)
-            }
-        val projectResults =
-            projectDao.searchProjectsGlobal(query).map {
-                GlobalSearchResultItem.ProjectItem(it)
-            }
-        val activityResults =
-            activityRepository.searchActivities(query).map {
-                GlobalSearchResultItem.ActivityItem(it)
-            }
-        val inboxResults =
-            inboxRecordDao.searchInboxRecordsGlobal(query).map {
-                GlobalSearchResultItem.InboxItem(it)
-            }
-
-        val combinedResults = (goalResults + linkResults + subprojectResults + projectResults + activityResults + inboxResults)
-
-        return combinedResults.sortedWith(
-            compareBy<GlobalSearchResultItem> { it.typeOrder }
-                .thenByDescending { it.timestamp },
-        )
+        return searchRepository.searchGlobal(query)
     }
+
 
     @Transaction
     suspend fun moveProject(
---
+++ app/src/main/java/com/romankozak/forwardappmobile/data/repository/SearchRepository.kt
@@ -0,0 +1,74 @@
+package com.romankozak.forwardappmobile.data.repository
+
+import com.romankozak.forwardappmobile.data.dao.GoalDao
+import com.romankozak.forwardappmobile.data.dao.InboxRecordDao
+import com.romankozak.forwardappmobile.data.dao.LinkItemDao
+import com.romankozak.forwardappmobile.data.dao.ListItemDao
+import com.romankozak.forwardappmobile.data.dao.ProjectDao
+import com.romankozak.forwardappmobile.data.database.models.GlobalSearchResultItem
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private val GlobalSearchResultItem.typeOrder: Int
+    get() =
+        when (this) {
+            is GlobalSearchResultItem.ProjectItem,
+            is GlobalSearchResultItem.SublistItem,
+            -> 0
+            is GlobalSearchResultItem.GoalItem -> 1
+            else -> 2
+        }
+
+@Singleton
+class SearchRepository
+@Inject
+constructor(
+    private val goalDao: GoalDao,
+    private val projectDao: ProjectDao,
+    private val listItemDao: ListItemDao,
+    private val linkItemDao: LinkItemDao,
+    private val activityRepository: ActivityRepository,
+    private val inboxRecordDao: InboxRecordDao,
+) {
+    suspend fun searchGlobal(query: String): List<GlobalSearchResultItem> {
+        val goalResults =
+            goalDao.searchGoalsGlobal(query).mapNotNull { searchResult ->
+                val listItem = listItemDao.getListItemByEntityId(searchResult.goal.id)
+                listItem?.let {
+                    GlobalSearchResultItem.GoalItem(
+                        goal = searchResult.goal,
+                        listItem = it,
+                        projectName = searchResult.projectName,
+                        pathSegments = searchResult.pathSegments,
+                    )
+                }
+            }
+        val linkResults =
+            linkItemDao.searchLinksGlobal(query).map {
+                GlobalSearchResultItem.LinkItem(it)
+            }
+        val subprojectResults =
+            projectDao.searchSubprojectsGlobal(query).map {
+                GlobalSearchResultItem.SublistItem(it)
+            }
+        val projectResults =
+            projectDao.searchProjectsGlobal(query).map {
+                GlobalSearchResultItem.ProjectItem(it)
+            }
+        val activityResults =
+            activityRepository.searchActivities(query).map {
+                GlobalSearchResultItem.ActivityItem(it)
+            }
+        val inboxResults =
+            inboxRecordDao.searchInboxRecordsGlobal(query).map {
+                GlobalSearchResultItem.InboxItem(it)
+            }
+
+        val combinedResults = (goalResults + linkResults + subprojectResults + projectResults + activityResults + inboxResults)
+
+        return combinedResults.sortedWith(
+            compareBy<GlobalSearchResultItem> { it.typeOrder }
+                .thenByDescending { it.timestamp },
+        )
+    }
+}
