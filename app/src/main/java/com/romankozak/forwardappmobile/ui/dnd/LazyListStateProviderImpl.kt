819e41c9b018a1ba8df10bc64776f6c00587fcfc
Roman Kozak <cossack.roman@gmail.com>
2025-10-25 19:14:27 +0300

    dnd працює. глюки з автоскрулом, першим елементом залишилися

--- app/src/main/java/com/romankozak/forwardappmobile/ui/dnd/LazyListStateProviderImpl.kt
+++ app/src/main/java/com/romankozak/forwardappmobile/ui/dnd/LazyListStateProviderImpl.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.LazyListState
 
 class LazyListStateProviderImpl(private val state: LazyListState) : LazyListInfoProvider {
+    private val itemHeightsMap = mutableMapOf<Int, Float>()
+
     override val lazyListItemInfo: List<LazyListItemInfo>
         get() = state.layoutInfo.visibleItemsInfo
 
@@ -13,12 +15,19 @@ class LazyListStateProviderImpl(private val state: LazyListState) : LazyListInfo
     override val viewportStartOffset: Int
         get() = state.layoutInfo.viewportStartOffset
 
-    override val firstVisibleItemIndex: Int
-        get() = state.firstVisibleItemIndex
-
     override val firstVisibleItemScrollOffset: Int
         get() = state.firstVisibleItemScrollOffset
 
     override val viewportSize: androidx.compose.ui.unit.IntSize
         get() = state.layoutInfo.viewportSize
+
+    override fun updateItemHeight(index: Int, height: Float) {
+        if (height > 0f) {
+            itemHeightsMap[index] = height
+        }
+    }
+
+    override fun getItemHeight(index: Int): Float? {
+        return itemHeightsMap[index]
+    }
 }
