d96340d35ef3f2bb3f4932646daaeb17ae94c027
Roman Kozak <cossack.roman@gmail.com>
2025-11-11 06:06:52 +0200

    listitems fixed

--- shared/src/commonMain/kotlin/com/romankozak/forwardappmobile/shared/features/projects/data/mappers/ListItemMapper.kt
+++ shared/src/commonMain/kotlin/com/romankozak/forwardappmobile/shared/features/projects/data/mappers/ListItemMapper.kt
@@ -1,7 +1,7 @@
 package com.romankozak.forwardappmobile.shared.features.projects.data.mappers
 
 import com.romankozak.forwardappmobile.shared.database.ListItems
-import com.romankozak.forwardappmobile.shared.data.models.ListItem
+import com.romankozak.forwardappmobile.shared.features.projects.data.models.ListItem
 
 fun ListItems.toDomain(): ListItem = ListItem(
     id = id,
