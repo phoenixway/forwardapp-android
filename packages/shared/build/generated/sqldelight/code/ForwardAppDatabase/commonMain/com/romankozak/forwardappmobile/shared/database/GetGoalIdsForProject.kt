76d523379f41e0c404ab40491522ee7bf80e7df6
Roman Kozak <cossack.roman@gmail.com>
2025-11-15 23:12:34 +0200

    Remove build artifacts

--- packages/shared/build/generated/sqldelight/code/ForwardAppDatabase/commonMain/com/romankozak/forwardappmobile/shared/database/GetGoalIdsForProject.kt
+++
@@ -1,7 +0,0 @@
-package com.romankozak.forwardappmobile.shared.database
-
-import kotlin.String
-
-public data class GetGoalIdsForProject(
-  public val entityId: String?,
-)
--- packages/shared/build/generated/sqldelight/code/ForwardAppDatabase/commonMain/com/romankozak/forwardappmobile/shared/features/attachments/types/checklists/Checklists.kt
+++
@@ -1,9 +0,0 @@
-package com.romankozak.forwardappmobile.shared.features.attachments.types.checklists
-
-import kotlin.String
-
-public data class Checklists(
-  public val id: String,
-  public val projectId: String,
-  public val name: String,
-)
