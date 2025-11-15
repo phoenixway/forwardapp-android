49ef5313cce0de376de167cc937b873f61c256a5
Roman Kozak <cossack.roman@gmail.com>
2025-11-15 13:40:30 +0200

    done

--- packages/shared/src/commonMain/kotlin/com/romankozak/forwardappmobile/shared/data/database/models/LinkItemEntity.kt
+++ packages/shared/src/commonMain/kotlin/com/romankozak/forwardappmobile/shared/data/database/models/LinkItemEntity.kt
@@ -1,5 +1,7 @@
 package com.romankozak.forwardappmobile.shared.data.database.models
 
+import com.romankozak.forwardappmobile.shared.data.models.RelatedLink
+
 data class LinkItemEntity(
     val id: String,
     val linkData: RelatedLink,
