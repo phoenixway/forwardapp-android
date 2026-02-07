28f6537fca25c200e07f5c384f916994a5214dc1
Roman Kozak <cossack.roman@gmail.com>
2026-02-07 22:43:35 +0200

    wip

--- app/src/main/java/com/romankozak/forwardappmobile/features/contexts/ui/context_screen/capabilities/direction/DirectionView.kt
+++ app/src/main/java/com/romankozak/forwardappmobile/features/contexts/ui/context_screen/capabilities/direction/DirectionView.kt
@@ -3,6 +3,7 @@ package com.romankozak.forwardappmobile.features.contexts.ui.context_screen.capa
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
