00ac1494c52e584d9ebdc5ce2a07adff79acae25
Roman Kozak <cossack.roman@gmail.com>
2026-02-14 13:51:36 +0000

    angelicaa fix4

--- app/src/main/java/com/romankozak/forwardappmobile/features/mainscreen/DashboardContent.kt
+++ app/src/main/java/com/romankozak/forwardappmobile/features/mainscreen/DashboardContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope.weight
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
