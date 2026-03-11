b42cd6eacf2bf863dd6ae0b4daa0c97d65cf75c2
Roman Kozak <cossack.roman@gmail.com>
2026-03-11 14:24:14 +0200

    chore(debt): reduce detekt findings in Log stub and ConnectionsPanel

--- app/src/test/java/android/util/Log.kt
+++ app/src/test/java/android/util/Log.kt
@@ -1,35 +1,46 @@
 package android.util
 
 object Log {
+    private fun stubResult(
+        tag: String?,
+        msg: String?,
+        tr: Throwable? = null,
+    ): Int {
+        val tagLen = tag?.length ?: 0
+        val msgLen = msg?.length ?: 0
+        val throwableMarker = if (tr != null) 1 else 0
+        return tagLen + msgLen + throwableMarker
+    }
+
     @JvmStatic
     fun d(
         tag: String?,
         msg: String?,
-    ): Int = 0
+    ): Int = stubResult(tag = tag, msg = msg)
 
     @JvmStatic
     fun e(
         tag: String?,
         msg: String?,
         tr: Throwable? = null,
-    ): Int = 0
+    ): Int = stubResult(tag = tag, msg = msg, tr = tr)
 
     @JvmStatic
     fun i(
         tag: String?,
         msg: String?,
-    ): Int = 0
+    ): Int = stubResult(tag = tag, msg = msg)
 
     @JvmStatic
     fun w(
         tag: String?,
         msg: String?,
-    ): Int = 0
+    ): Int = stubResult(tag = tag, msg = msg)
 
     @JvmStatic
     fun w(
         tag: String?,
         msg: String?,
         tr: Throwable?,
-    ): Int = 0
+    ): Int = stubResult(tag = tag, msg = msg, tr = tr)
 }
