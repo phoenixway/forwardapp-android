891ec9c7c1c5eee1beadb6c3a0d22dd36e4273e4
Roman Kozak <cossack.roman@gmail.com>
2026-03-11 19:24:08 +0000

    wip

--- app/src/main/java/com/romankozak/forwardappmobile/features/contexts/ui/context_screen/capabilities/backlog/BacklogItem.kt
+++ app/src/main/java/com/romankozak/forwardappmobile/features/contexts/ui/context_screen/capabilities/backlog/BacklogItem.kt
@@ -23,6 +23,7 @@ import androidx.compose.material.icons.filled.Flag
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
