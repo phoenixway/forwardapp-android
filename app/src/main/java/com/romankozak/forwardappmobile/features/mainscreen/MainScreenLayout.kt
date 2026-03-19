e234fa11389b9db3d074df7388d65f60a2caa2d2
Roman Kozak <cossack.roman@gmail.com>
2026-03-19 04:49:38 +0000

    wip

--- app/src/main/java/com/romankozak/forwardappmobile/features/mainscreen/MainScreenLayout.kt
+++ app/src/main/java/com/romankozak/forwardappmobile/features/mainscreen/MainScreenLayout.kt
@@ -476,8 +476,9 @@ fun MainScreenLayout(
                 )
 
                 if (currentRoute == MAIN_SCREEN_DASHBOARD_ROUTE) {
+                    Spacer(Modifier.height(6.dp))
                     dashboardHeaderContent()
-                    Spacer(Modifier.height(1.dp))
+                    Spacer(Modifier.height(4.dp))
                 } else {
                     Spacer(Modifier.height(16.dp))
                 }
